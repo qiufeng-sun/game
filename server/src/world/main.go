51aae8610b1eaa40de0c20bff3f548adccebaf5c
sunxiguang <sunxiguang@xiaomi.com>
2016-12-26 16:03:00 +0800

    update

--- server/src/world/main.go
+++ server/src/world/main.go
@@ -3,23 +3,14 @@ package main
 
 //
 import (
-	"runtime/debug"
-
-	"util/logs"
-
+	"core"
 	"core/server"
 	"world/world"
 )
 
 // 程序入口
 func main() {
-	defer func() {
-		if e := recover(); e != nil {
-			logs.GetLogger().Critical("panic:%v", e)
-			logs.Warn(string(debug.Stack()))
-		}
-		logs.Close()
-	}()
+	defer core.PrintPanic()
 
 	server.Run(world.New())
 }
